dd94bbeca261be5ceaf58fb53f239ef071cd031e
IVENTEYE <52000764+IVENTEYE@users.noreply.github.com>
2020-02-19 21:15:06 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let menu = document.querySelector(".menu__icon"),
+  menu__body = document.querySelector(".menu-body");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+menu.addEventListener("click", () => {
+  menu__body.classList.toggle("active");
+});
+
+$('.slider__body').slick({
+	arrows: false,
+	dots: true,
+});
+
+function ibg() {
+  let ibg = document.querySelectorAll(".ibg");
+  for (var i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector("img")) {
+      ibg[i].style.backgroundImage =
+        "url(" + ibg[i].querySelector("img").getAttribute("src") + ")";
+    }
+  }
+}
+
+ibg();
